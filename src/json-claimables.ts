d566d2ebbe39b76463e3ed85042e17c909934b80
Dawson Botsford <dawsonbotsford@gmail.com>
2021-02-06 22:01:52 -0700

    Add all POAP's

--- src/json-claimables.ts
+++ src/json-claimables.ts
@@ -6,50 +6,70 @@ type AirDrops = {
   [tokenName: string]: AirDrop;
 };
 export const jsonClaimables: AirDrops = {
-  eth2GenesisPOAP: {
+  // all poaps listed at https://github.com/poap-xyz/poap-delivery/tree/development/app/src/lib/events
+  // TODO: pull from this directory dynamically
+  "poap-aave-v2-pioneers": {
     url:
-      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/c28f37773a3d9a9be72a6936c7c6a2e55e2ec742/app/src/lib/events/eth2-genesis.json",
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/aave-v2-pioneers.json",
   },
-  // // tokens called "tokens" in csv
-  // "1inch": {
-  //   url:
-  //     "https://gist.githubusercontent.com/banteg/12708815fb63239d9f28dec5df8641f9/raw/28a9dffe9d5681ef5f75b0ab6c39fe5ea0064712/1inch.csv",
-  //   tokenNamed: "tokens",
-  // },
-  // // no csv header at all
-  // tornado: {
-  //   url:
-  //     "https://raw.githubusercontent.com/tornadocash/claimable/master/claimable.csv",
-  //   tokenNamed: "no-header",
-  // },
-  // // tokens called "badger" in csv
-  // badger: {
-  //   url:
-  //     "https://gist.githubusercontent.com/banteg/9ad5fdd2e169a03cc5d93478ece10a38/raw/9b14f2fd933d8a817ff6773e4d4854832b02c4b8/badger.csv",
-  //   tokenNamed: "badger",
-  // },
-  // // tokens called "tokens" in csv
-  // furucombo: {
-  //   url:
-  //     "https://gist.githubusercontent.com/nicholashc/c96d6b41e33d1245ecdaaea33fa6fab0/raw/05d3042e034c2ea99d7084789962aa95a3330f04/combo.csv",
-  //   tokenNamed: "tokens",
-  // },
-  // // tokens called "tokens" in csv
-  // stakeDAO: {
-  //   url:
-  //     "https://gist.githubusercontent.com/nicholashc/d380275aa8118e018906feeda3a92be5/raw/07f4a09dd657a27080cc75eda5e864acc030af5f/stakedao.csv",
-  //   tokenNamed: "tokens",
-  // },
-  // // tokens called "token" in csv
-  // curve: {
-  //   url:
-  //     "https://gist.githubusercontent.com/nicholashc/f4a34c138087195237556077ea6490d7/raw/bfdf0a9886747dfe3465a2e8ea1bfb02ae0386ac/curve.csv",
-  //   tokenNamed: " token",
-  // },
-  // // tokens called "token" in csv
-  // digg: {
-  //   url:
-  //     "https://gist.githubusercontent.com/nicholashc/c21788b0f0391d0d2d2cdcc44940a0e3/raw/0dbdc85e0ebc4d2aaa7cda49d5aaeb903fef69c7/digg.csv",
-  //   tokenNamed: " token",
-  // },
+  "poap-beacon-chain-first-1024": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/beacon-chain-first-1024.json",
+  },
+  "poap-beacon-chain-first-32769": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/beacon-chain-first-32769.json",
+  },
+  "poap-coin-gecko-yield-farming": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/coin-gecko-yield-farming.json",
+  },
+  "poap-eth2-genesis": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/eth2-genesis.json",
+  },
+  "poap-half-rekt": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/half-rekt.json",
+  },
+  "poap-keep-stakers": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/keep-stakers.json",
+  },
+  "poap-lumberjackers": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/lumberjackers.json",
+  },
+  "poap-medalla": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/medalla.json",
+  },
+  "poap-muir-glacier": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/muir-glacier.json",
+  },
+  "poap-proof-of-gucci-design-competition": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/proof-of-gucci-design-competition.json",
+  },
+  "poap-proof-of-gucci": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/proof-of-gucci.json",
+  },
+  "poap-resuscitators": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/resuscitators.json",
+  },
+  "poap-yam": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/yam.json",
+  },
+  "poap-ycover": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/ycover.json",
+  },
+  "poap-yfi-og": {
+    url:
+      "https://raw.githubusercontent.com/poap-xyz/poap-delivery/development/app/src/lib/events/yfi-og.json",
+  },
 };
